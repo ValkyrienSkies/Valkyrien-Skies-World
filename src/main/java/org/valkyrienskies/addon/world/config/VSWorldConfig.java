99a6611ed204b78321f774ed883dec2d3dee8f5a
Tri0de <best108@uw.edu>
2020-09-05 23:48:09 -0700

    Cleaned up VSWorldConfig

--- src/main/java/org/valkyrienskies/addon/world/config/VSWorldConfig.java
+++ src/main/java/org/valkyrienskies/addon/world/config/VSWorldConfig.java
@@ -6,39 +6,34 @@ import net.minecraftforge.fml.client.event.ConfigChangedEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.valkyrienskies.addon.world.ValkyrienSkiesWorld;
-import org.valkyrienskies.mod.common.config.VSConfig;
 import org.valkyrienskies.mod.common.config.VSConfigTemplate;
 
+// NOTE: When updating names/comments remember to update them in the lang files.
 @SuppressWarnings("WeakerAccess") // NOTE: Any forge config option MUST be "public"
 @Config(modid = ValkyrienSkiesWorld.MOD_ID)
 public class VSWorldConfig extends VSConfigTemplate {
 
-    @Config.Comment({
-            "Valkyrium Crystal Anti-Gravity force.",
-            "Default is 1. Set to 0 to disable."
-    })
-    public static double valkyriumCrystalForce = 1D;
-
-    @Config.Comment({
-            "Valkyrium Ore Anti-Gravity force.",
-            "1 is the same as a crystal, default is 4.",
-            "Set to 0 to disable."
-    })
-    public static double valkyriumOreForce = 4D;
-
+    @Config.Name("Enable Valkyrium Item Lift Force")
     @Config.Comment({
             "When true valkyrium items add upwards force to players holding them",
             "When false they won't."
     })
     public static boolean valkyriumItemsLiftPlayers = true;
 
+    @Config.Name("Valkyrium Crystal Anti-Gravity force")
+    @Config.Comment("Default is 1. Set to 0 to disable.")
+    public static double valkyriumCrystalForce = 1;
+
+    @Config.Name("Valkyrium Ore Anti-Gravity force")
+    @Config.Comment("Default is 4. Set to 0 to disable.")
+    public static double valkyriumOreForce = 4;
+
     /**
      * Synchronizes the data in this class and the data in the forge configuration
      */
     public static void sync() {
         ConfigManager.sync(ValkyrienSkiesWorld.MOD_ID, Config.Type.INSTANCE);
-
-        VSConfig.onSync();
+        VSWorldConfig.onSync();
     }
 
     @Mod.EventBusSubscriber(modid = ValkyrienSkiesWorld.MOD_ID)
